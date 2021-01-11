fbc7bb8260081f501eec3683fdcf35dfb342b408
mar y sol bautista <marilirulita@gmail.com>
2021-01-11 13:52:16 -0800

    Fix linters spaces

--- bubble.rb
+++ bubble.rb
@@ -8,12 +8,13 @@ def bubble_sort(test)
   test.length.times do
     # I need to compare the i number in array with i+1
     (0..test.length - 2).each do |i|
-    # if the number i is higher than i+1, then switch places, and continue checking
+      # if the number i is higher than i+1, then switch places, and continue checking
+
       next unless test[i] > test[i + 1]
         m = test[i]
         test[i] = test[i + 1]
         test[i + 1] = m
-      end
+    end
   end
   test
 end
@@ -27,17 +28,18 @@ def bubble_sort_by(test)
   test.length.times do
     # I need to compare the i number in array with i+1
     (0..test.length - 2).each do |i|
-    # compare element one to second, if difference is positive then switch places, and continue checking
+      # compare element one to second, if difference is positive then switch places, and continue checking
+
       next unless yield(test[i], test[i + 1]).positive?
         m = test[i]
         test[i] = test[i + 1]
         test[i + 1] = m
-      end
+    end
   end
   test
 end
 
-test_arra = bubble_sort_by(%w['hi', 'hey', 'hello']) do |left,right|
+test_arra = bubble_sort_by(%w[hi, hey, hello]) do |left, right|
   left.length - right.length
 end
 
