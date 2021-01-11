5ec3ebceb267b49d674a2313f2ede230fe9e0bc7
mar y sol bautista <marilirulita@gmail.com>
2021-01-11 13:29:52 -0800

    Add space in comments

--- bubble.rb
+++ bubble.rb
@@ -1,14 +1,14 @@
-#create a variable for store array to test
+# create a variable for store array to test
 test_array = [3, 4, 5, 9, 8, 2]
-#create method
+# create method
 def bubble_sort(test)
-  #variable to save element to change
+  # variable to save element to change
   m = 0
-  #create a loop for check n times the array
+  # create a loop for check n times the array
   test.length.times do
-    #I need to compare the i number in array with i+1
-    for i in 0..test.length-2
-    #if the number i is higher than i+1, then switch places, and continue checking
+    # I need to compare the i number in array with i+1
+    (0..test.length - 2).each do |i|
+    # if the number i is higher than i+1, then switch places, and continue checking
     if test[i] > test[i+1]
       m = test[i]
       test[i] = test[i+1]
@@ -22,13 +22,13 @@ end
 print bubble_sort(test_array)
 
 def bubble_sort_by(test)
-  #variable to save element to change
+  # variable to save element to change
   m = 0
-  #create a loop for check n times the array
+  # create a loop for check n times the array
   test.length.times do
-    #I need to compare the i number in array with i+1
+    # I need to compare the i number in array with i+1
     for i in 0..test.length-2
-    #compare element one to second, if difference is positive then switch places, and continue checking
+    # compare element one to second, if difference is positive then switch places, and continue checking
     if yield(test[i], test[i+1]).positive?
       m = test[i]
       test[i] = test[i+1]
