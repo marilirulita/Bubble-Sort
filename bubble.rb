158c4e6e67fa16eb564bb0fab5007ba9c354b562
mar y sol bautista <marilirulita@gmail.com>
2021-01-11 13:01:23 -0800

    Add aditional method to check results

--- bubble.rb
+++ bubble.rb
@@ -1,16 +1,14 @@
-#Build a method #bubble_sort that takes an array and returns a sorted array
 #create a variable for store array to test
 test_array = [3, 4, 5, 9, 8, 2]
-test_array2 = ["a", "c", "b"]
-
 #create method
 def bubble_sort(test)
-  x = test.length-2
+  #variable to save element to change
   m = 0
+  #create a loop for check n times the array
   test.length.times do
-    #I need to compare the i number in array with the next
-    for i in 0..x
-    #if the number i is higher than i+1, then change site, else continue checking
+    #I need to compare the i number in array with i+1
+    for i in 0..test.length-2
+    #if the number i is higher than i+1, then switch places, and continue checking
     if test[i] > test[i+1]
       m = test[i]
       test[i] = test[i+1]
@@ -18,7 +16,31 @@ def bubble_sort(test)
     end
     end
   end
-  p test
+  test
 end
 
-bubble_sort(test_array)
+print bubble_sort(test_array)
+
+def bubble_sort_by(test)
+  #variable to save element to change
+  m = 0
+  #create a loop for check n times the array
+  test.length.times do
+    #I need to compare the i number in array with i+1
+    for i in 0..test.length-2
+    #compare element one to second, if difference is positive then switch places, and continue checking
+    if yield(test[i], test[i+1]).positive?
+      m = test[i]
+      test[i] = test[i+1]
+      test[i+1] = m
+    end
+    end
+  end
+  test
+end
+
+test_arra = bubble_sort_by(["hi", "hey", "hello"]) do |left,right|
+  left.length - right.length
+end
+
+print test_arra
