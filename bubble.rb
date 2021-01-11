ab56cc4539e843ae3b189430015d92ccd7a81a59
mar y sol bautista <marilirulita@gmail.com>
2021-01-11 09:35:43 -0800

    create method

---
+++ bubble.rb
@@ -0,0 +1,24 @@
+#Build a method #bubble_sort that takes an array and returns a sorted array
+#create a variable for store array to test
+test_array = [3, 4, 5, 9, 8, 2]
+
+#create method
+def bubble_sort(test)
+  x = test.length-1
+  m = 0
+  for i in 0..x
+    #1.1 if the number i is higher than i+1, then change site, else continue checking
+    if test[i] > test[i+1]
+      m = test[i]
+      test[i] = test[i+1]
+      test[i+1] = m
+    end
+    print test
+  end
+  #for each cicle i should stop looking for the last item = arr.length - 1
+    #I need to compare the i number in array with the next
+
+  x -= 1
+end
+
+bubble_sort(test_array)
